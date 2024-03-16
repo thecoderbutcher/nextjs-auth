ef77d492fc95aa21549e0a31b1555b5c396a0815
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-03-16 03:33:32 -0300

    feat: add user register

--- actions/register.tsx
+++ actions/register.tsx
@@ -1,7 +1,10 @@
 'use server';
 
 import * as z from "zod";
+import bcrypt from 'bcrypt';
 import { RegisterSchema } from "@/schemas";
+import {db} from '@/lib/db';
+import {getUserByEmail} from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
@@ -9,5 +12,20 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     if(!validateFields.success) {
         return {error:'Invalid fields!'};
     }
-    return {success: 'Email sent!'};
+    const {email, password, name} = validateFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+    if(existingUser) return {error: "Email already in use!"}
+
+    await db.user.create({
+        data:{
+            name, 
+            email,
+            password: hashedPassword,
+        }
+    });
+
+    //TODO: send verification email
+    return {success: 'User created!'};
 }
