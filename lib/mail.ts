72a4138e42c0c9a54f501727107004c8a37c2238
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-05-14 02:14:05 -0300

    feat: verification email with token !

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,32 +2,31 @@ import nodemailer from 'nodemailer';
 
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
-    const {SMTP_EMAIL, SMPT_PASSWORD} = process.env;
+    const {SMTP_MAIL, SMTP_PASSWORD} = process.env;
 
     const transport = nodemailer.createTransport({
         service:'gmail',
         auth:{
-            user: SMTP_EMAIL,
-            pass: SMPT_PASSWORD,
+            user: SMTP_MAIL,
+            pass: SMTP_PASSWORD,
         },
     });
     try {
         const testResult = await transport.verify();
         console.log(testResult)
     } catch (error) {
-        console.log(error + "eto no anda")
+        console.error(error)
     }
 
     try {
         const sendResult = await transport.sendMail({
-            from: SMTP_EMAIL,
+            from: SMTP_MAIL,
             to: email,
             subject: "Confirm your email",
             html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
         })
-        console.log(sendResult);
     } catch (error) {
-        console.log(error + 'eto tampoco')
+        console.error(error)
     } 
 }
 
